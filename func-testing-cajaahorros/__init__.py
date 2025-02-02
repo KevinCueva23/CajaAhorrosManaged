ff593044c8a732a74fefd4c68d062d40fde3bbbb
KevinCueva23 <kevin.cueva@unach.edu.ec>
2025-02-01 21:13:30 -0500

    sin funciones xd

--- func-testing-cajaahorros/__init__.py
+++ func-testing-cajaahorros/__init__.py
@@ -32,20 +32,3 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         )
 
 
-def get_token():
-    
-    CLIENT_ID = os.getenv('CLIENT_ID')
-    CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-    TENANT_ID = os.getenv('TENANT_ID')
-    url = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"
-    body = {
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-        "scope": "https://graph.microsoft.com/.default",
-        "grant_type": "client_credentials"
-    }
-    response = requests.post(url, data=body)
-    return response.json().get("access_token")
-
-
- 
