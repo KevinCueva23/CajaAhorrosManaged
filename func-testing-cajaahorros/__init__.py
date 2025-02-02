5fb0015fc667cc9b2f320b67b9c79c9a94b64170
KevinCueva23 <kevin.cueva@unach.edu.ec>
2025-02-01 20:31:18 -0500

    claves

--- func-testing-cajaahorros/__init__.py
+++ func-testing-cajaahorros/__init__.py
@@ -18,11 +18,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
+        print("Tenant:",os.environ['TENANT_ID'])
+        print("Client:",os.environ['CLIENT_ID'])
+        print("Secret:",os.environ['CLIENT_SECRET'])
         return func.HttpResponse(
              "Esta cosa no funciona :( pipipi)",
-             print("Tenant:",os.environ['TENANT_ID']),
-             print("Client:",os.environ['CLIENT_ID']),
-             print("Secret:",os.environ['CLIENT_SECRET']),
              status_code=200
         )
  
