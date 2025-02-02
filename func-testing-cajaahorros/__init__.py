1b43b06903d2c71762221b046091a52f1fe09bab
KevinCueva23 <kevin.cueva@unach.edu.ec>
2025-02-01 21:18:23 -0500

    sin ese hola :c

--- func-testing-cajaahorros/__init__.py
+++ func-testing-cajaahorros/__init__.py
@@ -11,7 +11,6 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    print("Hola")
     name = req.params.get('name')
     if not name:
         try:
