84d0d9bc635ea1e60cca12453922104e52460651
KevinCueva23 <kevin.cueva@unach.edu.ec>
2025-02-01 21:00:25 -0500

    sin funcion

--- func-testing-cajaahorros/__init__.py
+++ func-testing-cajaahorros/__init__.py
@@ -28,60 +28,6 @@ def get_token():
     return response.json().get("access_token")
 
 
-def defender_agents_report():
-    auth_token = get_token()
-
-    headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': "Bearer " + auth_token
-    }
-
-    body = {
-        "reportName": "DefenderAgents",
-        "select": [
-            "DeviceId", "_ManagedBy", "DeviceName", "DeviceState", "PendingFullScan", "PendingReboot",
-            "PendingManualSteps", "PendingOfflineScan", "CriticalFailure",
-            "MalwareProtectionEnabled", "RealTimeProtectionEnabled", "NetworkInspectionSystemEnabled",
-            "SignatureUpdateOverdue", "QuickScanOverdue", "FullScanOverdue", "RebootRequired",
-            "FullScanRequired", "EngineVersion", "SignatureVersion", "AntiMalwareVersion",
-            "LastQuickScanDateTime", "LastFullScanDateTime", "LastQuickScanSignatureVersion",
-            "LastFullScanSignatureVersion", "LastReportedDateTime", "UPN", "UserEmail", "UserName"
-        ]
-    }
-
-    response = requests.post("https://graph.microsoft.com/v1.0/deviceManagement/reports/exportJobs", 
-                             data=json.dumps(body), headers=headers)
-    report_id = response.json().get('id')
-
-    status = ""
-    while status != "completed":
-        url = f"https://graph.microsoft.com/beta/deviceManagement/reports/exportJobs('{report_id}')"
-        response = requests.get(url, headers=headers)
-        status = response.json().get('status')
-        print(f"Esperando... Estado: {status}")
-
-    # Obtener la URL de descarga del informe
-    download_url = response.json().get('url')
-    report_response = requests.get(download_url)
-
-    # Descomprimir el archivo ZIP
-    with zipfile.ZipFile(io.BytesIO(report_response.content)) as zip_file:
-        # Extraemos el contenido del ZIP y lo leemos como CSV
-        with zip_file.open(zip_file.namelist()[0]) as csv_file:
-            lines = csv_file.read().decode('utf-8').splitlines()
-            rows = list(csv.reader(lines))
-
-    # Verificar si todas las filas tienen la misma cantidad de columnas que la primera
-    header_length = len(rows[0])
-    valid_rows = [row for row in rows[1:] if len(row) == header_length]
-
-    # Crear el diccionario con las filas válidas
-    result_dict = {row[0]: dict(zip(rows[0], row)) for row in valid_rows}
-
-    # Imprimir directamente el resultado en consola
-    print(json.dumps(result_dict, indent=4))
-    return json.dumps(result_dict, indent=4)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
